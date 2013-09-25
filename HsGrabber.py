ba812476329ed934fd13e846852bab54476eca90
Dabid Heereman <dheereman@gmail.com>
2013-09-25 15:22:14 +0000

    updated HsGrabber

--- HsGrabber.py
+++ HsGrabber.py
@@ -4,49 +4,26 @@
 #Hit Spool Grabber to be run on access
 #author: dheereman
 #
-'''
-For grabbing hs data from hubs independent (without sndaq providing alert).
-HsGrabber sends json to HsPublisher to grab hs data from hubs.
-Uses the Hs Interface infrastructure.
 
-Needs HsPublisher on expcont and HsWorker on hub running!!
-'''
 from datetime import datetime, timedelta
 import sys
 import getopt
 import zmq
 
-def usage():
-    print >>sys.stderr, """
-    usage :: HsGrabber.py [options]
-            -b         | begin of data "YYYY-MM-DD UTC HH:mm:ss"
-            -e         | end of data "YYYY-MM-DD UTC HH:mm:ss"
-            -c         | copydir e.g. "/mnt/data/pdaqlocal/HsDataCopy/"
-    """
-    sys.exit(1)
-        
-##take arguments from command line and check for correct input
-opts, args = getopt.getopt(sys.argv[1:], 'hb:e:c:', ['help','alert='])
-for o, a in opts:
-    if o == '-b':
-        alert_begin = datetime.strptime(str(a), "%Y-%m-%d UTC %H:%M:%S.%f")
-    if o == '-e':
-        alert_end = datetime.strptime(str(a), "%Y-%m-%d UTC %H:%M:%S.%f")
-    if o == '-c':
-        copydir = str(a)
-    elif o == '-h' or o == '--help':
-        usage()       
-if len(sys.argv) < 5 :
-    print usage()
 # build 0MQ sockets    
 context = zmq.Context()
-# Socket to receive alert message
+# Socket to send alert message
 grabber = context.socket(zmq.REQ)
-grabber.connect("tcp://expcont:55557")   #connection = tcp, host = bond0 on spts-expcont ip , port 
+grabber.connect("tcp://localhost:55557")   #connection = tcp, host = localhost ip , port 
+
+# Socket for I3Live on expcont
+i3socket = context.socket(zmq.PUSH) # former ZMQ_DOWNSTREAM is depreciated in recent releases, use PUSH instead
+i3socket.connect("tcp://localhost:6668") 
+print "connected to i3live socket on port 6668"    
 
 #class MyGrabber(object):
 def send_alert():
-    print "connect REQ socket for  sendingalert messages to port 55557 on expcont"
+    print "connect PUSH socket for sending alert messages to port 55557 on expcont"
     daqyearstart = datetime.strptime(str(alert_begin.year)+"-01-01", "%Y-%m-%d")
     alert_begin_delta = alert_begin - daqyearstart
     print "alert_begin_delta: " , alert_begin_delta
@@ -67,16 +44,63 @@ def send_alert():
     print "alert_dict to string looks like: " , alert_msg
     grabber.send(alert_msg)
     print "HsGrabber sent his Request"
-    answer = grabber.recv()
-    print "HsGrabber got the answer: " , answer , "from the HsPublisher"
-    print "HsWorkers should now be processing your request..."
+    try:
+        pubanswer = grabber.recv()
+        print "succesfully to HsPublisher: ", pubanswer
+    except Exception, err:
+        print "without success to HsPublisher: " , err
+    i3socket.send_json({"service": "HSiface", 
+                    "varname": "HsGrabber", 
+                    "value": "pushed request"})
+    sys.exit()
+    
+    
+    
 
 
 if __name__=="__main__":
+    '''
+    For grabbing hs data from hubs independent (without sndaq providing alert).
+    HsGrabber sends json to HsPublisher to grab hs data from hubs.
+    Uses the Hs Interface infrastructure.
+    
+    Needs HsPublisher on expcont and HsWorker on hub running!!
+    '''
+    def usage():
+        print >>sys.stderr, """
+        usage :: HsGrabber.py [options]
+            -b         | begin of data "YYYY-MM-DD HH:mm:ss.[subsec]"    subsec = 6digits
+            -e         | end of data "YYYY-MM-DD HH:mm:ss.[subsec]"      subsec = 6digits
+            -c         | copydir e.g. "pdaq@2ndbuild:/mnt/data/pdaqlocal/HsDataCopy/"
+            
+            HsGrabber reads UTC timestamps and converts them to sndaq time units [nanoseconds from beginning ofthe year]. This is 
+            in analogy to the message format that sndaq, as a first user of the hitspool interface, is sending.
+        """
+        sys.exit(1)
+        
+    ##take arguments from command line and check for correct input
+    opts, args = getopt.getopt(sys.argv[1:], 'hb:e:c:', ['help','alert='])
+    for o, a in opts:
+        if o == '-b':
+            try:
+                alert_begin = datetime.strptime(str(a), "%Y-%m-%d %H:%M:%S.%f")
+            except ValueError, e:
+                print "Problem with the time-stamp format: ", e
+                try:
+                    print "Try matching format without microsecond precision:"
+                    alert_begin = datetime.strptime(str(a), "%Y-%m-%d %H:%M:%S")
+                    print "matched"
+                except ValueError,e:
+                    print  "Problem with the time-stamp format: ", e
+            else:
+                pass
+        if o == '-e':
+            alert_end = datetime.strptime(str(a), "%Y-%m-%d %H:%M:%S.%f")
+        if o == '-c':
+            copydir = str(a)
+        elif o == '-h' or o == '--help':
+            usage()       
+    if len(sys.argv) < 5 :
+        print usage()
+    
     send_alert()
-    
-    
-#x = MyGrabber()
-#x.send_alert()
-
-
