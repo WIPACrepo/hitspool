f0208766dc89a3aadce28291c60d71fc58cb8124
Dabid Heereman <dheereman@gmail.com>
2013-11-01 14:42:46 +0000

    use python26 alias on SPS

--- run_worker_bkg.sh
+++ run_worker_bkg.sh
@@ -8,11 +8,11 @@ HSiface_PATH=$1
 #echo $HSiface_PATH/HsWorker.py
 
 #start the Hsinterface compnonent in BACKGROUND (" &" added at EOL!) and redirect stderr and stdout
-python $HSiface_PATH"HsWorker.py" >$HSiface_PATH"hsworker_stderr_stdout.log" 2>&1 &
+python26 $HSiface_PATH"HsWorker.py" >$HSiface_PATH"hsworker_stderr_stdout.log" 2>&1 &
 
 #wait with Watcher for some seconds to give Worker time to set up his connections...
 #sleep 3
 
 #now check the status of Worker:
 #echo "run watcher"
-python $HSiface_PATH"HsWatcher.py"
+python26 $HSiface_PATH"HsWatcher.py"
