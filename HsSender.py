c8e93ea778dba4e30aacf9a542b487bdc3fa6620
Dave Glowacki <dglo@icecube.wisc.edu>
2016-03-24 21:14:21 +0000

    Remove copied directory after it has been queued for SPADE

--- HsSender.py
+++ HsSender.py
@@ -745,7 +745,13 @@ class HsSender(HsBase):
                           " directory. Use HsSpader.py, for example.")
             return None
 
-        logging.info("Preparation for SPADE Pickup of %s DONE", copydir)
+        try:
+            shutil.rmtree(copydir)
+        except:
+            logging.exception("Could not remove copied directory \"%s\"" %
+                              copydir)
+
+        logging.info("SPADE file is %s", hs_bzipname)
 
         return (hs_bzipname, hs_spade_semfile)
 
